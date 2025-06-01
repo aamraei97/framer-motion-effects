b896604a22f7ee9868d1057d1af3a78f91f9765f
Arman Amraei <aamraei97@gmail.com>
2025-06-01 12:48:08 +0330

    ci: github pages config

--- src/app/1/_components/motion-item.tsx
+++ src/app/1/_components/motion-item.tsx
@@ -1,7 +1,7 @@
 import { motion, MotionValue, useTransform, useMotionValueEvent } from "motion/react";
 import { useMemo, useState } from "react";
 
-export function MotionItem({ index, y, color }: { index: number, y: MotionValue<number>, color: string }) {
+export function MotionItem({ index, y }: { index: number, y: MotionValue<number> }) {
     const test = useTransform(y, [0, Math.abs(0.12 + (index * 0.25))], [`${115 + (index * 70)}%`, `${15 + (index * 2)}%`])
     const rotateValue = useMemo(() => index % 2 === 0 ? Math.random() * 5 : -1 * Math.random() * 5, [index])
     const [top, setTop] = useState("100%")
